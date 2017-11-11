Fix modified gradient variable by inplace operation error in discriminator for latest pytorch build (from source)

GANs/pixelda_gan.py
@@ -108,7 +108,7 @@ class conv_block(nn.Module):
             # batch_size x in_channels x H x W
             nn.Conv2d(in_channels, out_channels, kernel_size=kernel_size, stride=stride, padding=padding, bias=False),
             nn.BatchNorm2d(out_channels),
-            nn.LeakyReLU(leakiness, inplace=True)
+            nn.LeakyReLU(leakiness, inplace=False)
             # batch_size x out_channels x H' x W'
         )
 
@@ -128,7 +128,7 @@ class inject_noise(nn.Module):
         self.noise_stddev = opt.D_noise_stddev
         self.dropout = nn.Sequential()
         if dropout:
-            self.dropout = nn.Dropout(opt.D_keep_prob, inplace=True)
+            self.dropout = nn.Dropout(opt.D_keep_prob, inplace=False)
         self.noise = torch.FloatTensor()
         if opt.cuda:
             self.noise = self.noise.cuda()
@@ -160,7 +160,7 @@ class pixelda_D(nn.Module):
             # batch_size x in_channels x 64 x 64
             nn.Conv2d(in_channels, opt.ndf, kernel_size=3, stride=1, padding=1, bias=True),
             # nn.BatchNorm2d(opt.ndf),
-            nn.LeakyReLU(opt.leakiness, inplace=True),
+            nn.LeakyReLU(opt.leakiness, inplace=False),
             *layers
         )
         self.fully_connected = nn.Linear(projection_size*projection_size*out_channels, 1)